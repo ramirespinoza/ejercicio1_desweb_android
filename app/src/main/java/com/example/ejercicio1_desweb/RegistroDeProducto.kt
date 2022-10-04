e962efe6b2b06a318ce17aef7ba583e9f9591ec4
Carlos Ramirez <ramirespinoza@gmail.com>
2022-10-04 02:14:39 -0600

    Se completa el formulario del Activity RegistroDeProducto.kt y se crean las clases Productos(dentro del paquete modelo), Assets(dentro del paquete utilidades) y Connection para la conexión a SQLite y almacenar productos en ella.

--- app/src/main/java/com/example/ejercicio1_desweb/RegistroDeProducto.kt
+++ app/src/main/java/com/example/ejercicio1_desweb/RegistroDeProducto.kt
@@ -1,29 +1,63 @@
 package com.example.ejercicio1_desweb
 
+import android.content.ContentValues
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
+import com.example.ejercicio1_desweb.utilidades.Assets
+//import com.example.ejercicio1_desweb.Connection
 
 
 class RegistroDeProducto : AppCompatActivity() {
+
+    lateinit var codigo :EditText
+    lateinit var nombre :EditText
+    lateinit var precio :EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro_de_producto)
-        //this.codigo = findViewById<View>(R.id.textView2)
+        this.codigo = findViewById<View>(R.id.editTextCodigoProducto) as EditText
+        this.nombre = findViewById<View>(R.id.editTextNombreProducto) as EditText
+        this.precio = findViewById<View>(R.id.editTextPrecioProducto) as EditText
     }
 
-/*
+
     fun registrarProducto(){
         try {
-            var conexion = Connection(this, Assets.DB_NAME, null, Assets)
+            var conexion = Connection(this, Assets.DB_NAME, null, Assets.DB_VERSION)
             var sqliteDataBase  = conexion.writableDatabase
-            var values = ConetentValues
+            var values = ContentValues()
+            values.put(Assets.CAMPO_CODIGO_PRODUCTO, this.codigo.text.toString())
+            values.put(Assets.CAMPO_NOMBRE_PRODUCTO, this.nombre.text.toString())
+            values.put(Assets.CAMPO_PRECIO_PRODUCTO, this.precio.text.toString())
+
+            var result :Long = sqliteDataBase.insert(Assets.TABLA_PRODUCTO, Assets.CAMPO_CODIGO_PRODUCTO, values)
+
+            codigo.text.clear()
+            nombre.text.clear()
+            precio.text.clear()
+
+            Toast.makeText(applicationContext, "Producto Registrado", Toast.LENGTH_SHORT).show()
+
 
         } catch (e :Exception) {
-            Toast.makeText(applicationContext, "", Toast.LENGTH_SHORT).show();
+            Toast.makeText(applicationContext, "Error", Toast.LENGTH_SHORT).show()
         }
     }
 
- */
+    fun onClick(view :View) {
+        //var intent : Intent? = null
+        when(view.getId()){
+            R.id.btnRegresar -> {
+                //intent = Intent(this, MainActivity::class.java)
+                onBackPressed()
+            }
+            R.id.btnGuardar -> {this.registrarProducto()}
+        }
+        //if(intent!=null) { startActivity(intent) }
+    }
 }
