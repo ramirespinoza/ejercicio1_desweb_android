638527cef5a9785e73c9643ab9388725509c6ff8
Carlos Ramirez <ramirespinoza@gmail.com>
2022-10-06 19:04:59 -0600

    Se agrega el archivo para el Activity de listado de Productos con RecyclerView y se quita en EditText de más del layout de ListadoDeProductos.kt

---
+++ app/src/main/java/com/example/ejercicio1_desweb/ListadoDeProductosRV.kt
@@ -0,0 +1,11 @@
+package com.example.ejercicio1_desweb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListadoDeProductosRV : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listado_de_productos_rv)
+    }
+}
